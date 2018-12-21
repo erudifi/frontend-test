c54b93941d17b0d5d51cac27e97d2dd24ae457e3
Arnold Sebastian <arnold_sebastian_16@hotmail.com>
2018-12-21 09:15:42 +0700

    added getProvince, getCities, getDistricts, getSubDistricts inside locationService

--- src/services/locationService.js
+++ src/services/locationService.js
@@ -1,7 +1,35 @@
+import axios from 'axios';
+
 export default {
-	getLocation
+	getLocation,
+	getProvince,
+	getCities,
+	getDistricts,
+	getSubDistricts
 }
 
 function getLocation() {
-	window.
+	const sucess = position => {
+		return console.log(position)
+	};
+
+	const fail = error => {
+		return console.error(error)
+	};
+
+	window.navigator.geolocation.getCurrentPosition(sucess, fail)
+}
+
+function getProvince() {
+	return axios.get('https://danacita.co.id/api/locations/v1/provinces')
+}
+
+function getCities(id){
+	return axios.get(`https://danacita.co.id/api/locations/v1/cities?province_id=${id}`)
+}
+function getDistricts(id){
+	return axios.get(`https://danacita.co.id/api/locations/v1/districts?city_id=${id}`)
+}
+function getSubDistricts(id){
+	return axios.get(`https://danacita.co.id/api/locations/v1/subdistricts?district_id=${id}`)
 }
