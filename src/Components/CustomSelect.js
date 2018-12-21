a6e0c5bde70c344747e89b7bf310b52d1050522a
Arnold Sebastian <arnold_sebastian_16@hotmail.com>
2018-12-21 21:09:19 +0700

    Added proptypes and React.memo to gain performance

--- src/Components/CustomSelect.js
+++ src/Components/CustomSelect.js
@@ -1,14 +1,14 @@
-import React, { memo } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Select from 'react-select';
 
-const CustomSelect = ({ label, value, options, handleChange, isLoading }) => {
+const CustomSelect = ({ label, value, options, handleChange, isLoading, isDisabled }) => {
 	return (
 		<div>
 			<h3>{label}</h3>
 			<Select
 				value={value}
-				isDisabled={isLoading}
+				isDisabled={isDisabled}
 				options={options}
 				onChange={handleChange}
 				isLoading={isLoading}
@@ -21,11 +21,17 @@ CustomSelect.propTypes = {
 	label: PropTypes.string.isRequired,
 	value: PropTypes.shape({
 		label: PropTypes.string,
-		value: PropTypes.string
+		value: PropTypes.number
 	}).isRequired,
-	options,
-	handleChange,
-	isLoading
+	isDisabled: PropTypes.bool.isRequired,
+	options: PropTypes.arrayOf(
+		PropTypes.shape({
+			label: PropTypes.string,
+			value: PropTypes.number
+		}).isRequired
+	),
+	handleChange: PropTypes.func.isRequired,
+	isLoading: PropTypes.bool,
 };
 
-export default memo(CustomSelect);
+export default React.memo(CustomSelect);
