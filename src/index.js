5253a9f8420a2f2b9f52a22687aa4486a7f07267
Arnold Sebastian <arnold_sebastian_16@hotmail.com>
2018-12-26 00:12:26 +0700

    added hashrouter to support non client side router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
 	<Router basename={process.env.PUBLIC_URL}>
