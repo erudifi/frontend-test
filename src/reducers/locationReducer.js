d1b2ec52250028c8f7b8074abc7f5c9af5746f6d
Arnold Sebastian <arnold_sebastian_16@hotmail.com>
2018-12-27 23:42:40 +0700

    created and added actionTypes, actions and locationReducer for get province, cities, districts and subdistricts, upon request, success and fail

--- src/reducers/locationReducer.js
+++ src/reducers/locationReducer.js
@@ -1,12 +1,59 @@
+import locationActionTypes from '../actionTypes/locationActionTypes';
+
 const defaultState = {
 	provinces: [],
 	cities: [],
 	districts: [],
 	subDistricts: [],
+	isLoading: false,
+	isError: false,
+	error: '',
 };
 
 export default (state = defaultState, action) =>{
 	switch (action.type){
+		case locationActionTypes.GET_PROVINCES_REQUEST:
+		case locationActionTypes.GET_CITIES_REQUEST:
+		case locationActionTypes.GET_DISTRICTS_REQUEST:
+		case locationActionTypes.GET_SUB_DISTRICTS_REQUEST:
+			return {
+				...state,
+				isLoading: true
+			};
+		case locationActionTypes.GET_PROVINCES_SUCCESS:
+			return {
+				...state,
+				provinces: action.payload,
+				isLoading: false
+			};
+		case locationActionTypes.GET_CITIES_SUCCESS:
+			return {
+				...state,
+				cities: action.payload,
+				isLoading: false
+			};
+		case locationActionTypes.GET_DISTRICTS_SUCCESS:
+			return {
+				...state,
+				districts: action.payload,
+				isLoading: false
+			};
+		case locationActionTypes.GET_SUB_DISTRICTS_SUCCESS:
+			return {
+				...state,
+				subDistricts: action.payload,
+				isLoading: false
+			};
+		case locationActionTypes.GET_PROVINCES_FAIL:
+		case locationActionTypes.GET_CITIES_FAIL:
+		case locationActionTypes.GET_DISTRICTS_FAIL:
+		case locationActionTypes.GET_SUB_DISTRICTS_FAIL:
+			return {
+				...state,
+				isLoading: false,
+				isError: true,
+				error: action.payload
+			};
 		default: return state
 	}
 }
